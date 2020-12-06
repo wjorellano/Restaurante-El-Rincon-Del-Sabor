057185c670e7acf495bd686e88757221cbfdd93b
wilman orellano heredia <wilmanjuno2001@gmail.com>
2020-12-06 15:46:23 -0500

    ambio de base de datos

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_06_171406) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "nombre"
     t.string "descripcion"
@@ -50,7 +53,7 @@ ActiveRecord::Schema.define(version: 2020_12_06_171406) do
     t.integer "precio"
     t.integer "existencia"
     t.string "descripcion"
-    t.integer "category_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_dishes_on_category_id"
@@ -59,7 +62,7 @@ ActiveRecord::Schema.define(version: 2020_12_06_171406) do
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
@@ -69,8 +72,8 @@ ActiveRecord::Schema.define(version: 2020_12_06_171406) do
 
   create_table "sale_details", force: :cascade do |t|
     t.integer "cantidad"
-    t.integer "sale_id"
-    t.integer "dish_id"
+    t.bigint "sale_id"
+    t.bigint "dish_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "preciot"
@@ -80,8 +83,8 @@ ActiveRecord::Schema.define(version: 2020_12_06_171406) do
 
   create_table "sales", force: :cascade do |t|
     t.integer "total", default: 0
-    t.integer "client_id"
-    t.integer "user_id"
+    t.bigint "client_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "sale_details_id"
@@ -114,11 +117,16 @@ ActiveRecord::Schema.define(version: 2020_12_06_171406) do
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
+    t.bigint "user_id"
+    t.bigint "role_id"
     t.index ["role_id"], name: "index_users_roles_on_role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
+  add_foreign_key "dishes", "categories"
+  add_foreign_key "sale_details", "dishes"
+  add_foreign_key "sale_details", "sales"
+  add_foreign_key "sales", "clients"
+  add_foreign_key "sales", "users"
 end
